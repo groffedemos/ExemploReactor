b9c4574f81705d7453150fbf555db675fccb778d
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-12 20:36:49 -0300

    Segundo commit

--- SiteLiveReactor/Controllers/HomeController.cs
+++ SiteLiveReactor/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ public class HomeController : Controller
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _logger.LogInformation("Teste...");
     }
 
     public IActionResult Index()
